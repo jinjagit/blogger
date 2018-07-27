2bfc867941682fdd217d2298a5d865b428ea0ffc
Simon Tharby <jinjamail@gmail.com>
2018-07-27 16:44:06 +0100

    form-based workflow is complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,12 +17,16 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' deleted!"
 
     redirect_to articles_path(@articles)
   end
@@ -34,6 +38,8 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' updated!"
 
     redirect_to article_path(@article)
   end
