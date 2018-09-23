ad4ca7755d3566140c4acca7399838326d3305b2
Simon Tharby <jinjamail@gmail.com>
2018-09-23 14:31:27 +0100

    implement tagging

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,8 @@
 class CommentsController < ApplicationController
-  
+
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
-
     @comment.save
 
     flash.notice = "Comment created!"
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  
+  resources :tags
+
 end
