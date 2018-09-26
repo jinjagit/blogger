f2f08bb08c49b2fcbaf0392d78a71a1ca9835977
Simon Tharby <jinjamail@gmail.com>
2018-09-26 19:38:46 +0100

    add basic comment delete function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,16 @@ class CommentsController < ApplicationController
     redirect_to article_path(@comment.article)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @article = Article.find(params[:article_id])
+
+    flash.notice = "Comment deleted!"
+
+    redirect_to article_path(@article)
+  end
+
   def comment_params
     params.require(:comment).permit(:author_name, :body)
   end
